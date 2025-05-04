485fe696077cf763d867c65de9f873ef87c417a2
Ankushdhull9908 <123265836+Ankushdhull9908@users.noreply.github.com>
2025-05-05 00:38:15 +0530

    Update LoginForm.jsx

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useCart } from '../Context'
 import { asstes } from '../assets/assets'
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 
 function LoginForm() {
     const navigate= useNavigate()
@@ -74,7 +74,7 @@ function LoginForm() {
   return (
     
       <div className="loginform">
-        <ToastContainer />
+        
 
 <div className="formlogo">
   <img src={asstes.instagram} alt='instagram'/>
