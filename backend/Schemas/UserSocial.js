962e657314460e89c5f1b4ebae2c31f9e05e255f
Ankushdhull9908 <123265836+Ankushdhull9908@users.noreply.github.com>
2025-05-10 17:39:42 +0530

    Update UserSocial.js

--- backend/Schemas/UserSocial.js
+++ backend/Schemas/UserSocial.js
@@ -29,7 +29,7 @@ export const userSocialSchema = new mongoose.Schema({
     userId: {
         type: String,
         required: true,
-        unique: true,
+        
     },
     userprofile:{
         type:String,
@@ -39,7 +39,7 @@ export const userSocialSchema = new mongoose.Schema({
     Uname: {
         type: String,
         required: true,
-        unique:true
+        
     },
     AccountType: {
         type: String,
